2d899a34dbda1343c28a41e16a84dace3dd99856
resbayfandiyya <resbayfandiyyagautama@gmail.com>
2026-06-27 04:45:55 +0700

    Connect Railway MySQL

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,25 @@ const mysql = require("mysql2");
 
 const connection = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+connection.getConnection((err, conn) => {
+  if (err) {
+    console.log("❌ Database gagal terhubung");
+    console.log(err.message);
+    return;
+  }
+
+  console.log("✅ Database Railway Connected");
+  conn.release();
 });
 
 module.exports = connection.promise();
